aec28b55f2247fbdc27e62754cc669a5012d5955
AthaSSiN <athassin.asp@gmail.com>
2025-04-25 14:08:54 -0400

    make code faster

--- src/utils.py
+++ src/utils.py
@@ -18,7 +18,7 @@ def print_text_animated(text):
     # print(text)
     for char in text:
         print(char, end="", flush=True)
-        time.sleep(0.02)
+        # time.sleep(0.02)
 
 
 # COLOR = [Fore.BLUE, Fore.GREEN, Fore.YELLOW, Fore.RED, Fore.LIGHTGREEN_EX, Fore.CYAN]
